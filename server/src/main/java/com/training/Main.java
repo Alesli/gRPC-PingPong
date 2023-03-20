6fad50d1d51ff870977a494f95a45b5970f62b3d
Alesia_Skarakhod <Alesia.Skarakhod@wolterskluwer.com>
2023-03-21 00:07:59 +0200

    add logs

--- server/src/main/java/com/training/Main.java
+++ server/src/main/java/com/training/Main.java
@@ -1,16 +1,26 @@
 package com.training;
 
+import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        var server = ServerBuilder.forPort(8080)
-                .addService(new PingPongServer())
-                .build();
-        server.start();
-        server.awaitTermination();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws InterruptedException {
+        Server server = null;
+        try {
+            server = ServerBuilder.forPort(8080)
+                    .addService(new PingPongServer())
+                    .build()
+                    .start();
+            server.awaitTermination();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Server didn't start", e);
+        }
     }
 }
